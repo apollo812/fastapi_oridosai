bee0daefca0628f1b11ea19da36187b2ca8b4c36
apollo812 <goodluckbusiness0101@gmail.com>
2023-11-28 03:57:59 -0500

    feat: refiner

--- api/scripts/img2img.py
+++ api/scripts/img2img.py
@@ -13,33 +13,7 @@ def img2img(param: Img2ImgRequest, image):
     model = load_sdxl_refiner_model()
     
     init_image = load_image(image).convert("RGB")
-    # image = model(param.prompt, image=init_image).images[0]
     # Generate an image based on the image with input text prompts and other parameters using the loaded model.
-    result = model(
-        prompt = param.prompt,
-        prompt2 = param.prompt2,
-        image = init_image,
-        strength = param.strength,
-        num_inference_steps = param.num_inference_steps,
-        denoising_start = param.denoising_start,
-        denoising_end = param.denoising_end,
-        guidance_scale = param.guidance_scale,
-        negative_prompt = param.negative_prompt,
-        negative_prompt_2 = param.negative_prompt_2,
-        num_images_per_prompt = param.num_images_per_prompt,
-        eta = param.eta,
-        output_type = param.output_type,
-        return_dict = param.return_dict,
-        guidance_rescale = param.guidance_rescale,
-        # original_size = param.original_size,
-        # crops_coords_top_left = param.crops_coords_top_left,
-        # target_size = param.target_size,
-        # negative_original_size = param.negative_original_size,
-        # negative_crops_coords_top_left = param.negative_crops_coords_top_left,
-        # negative_target_size = param.negative_target_size,
-        aesthetic_score = param.aesthetic_score,
-        negative_aesthetic_score = param.negative_aesthetic_score,
-        clip_skip = param.clip_skip
-    )
-
-    return result[0][0]                    # Return the generated image.
+    image = model(param.prompt, image=init_image).images[0]
+    
+    return image                    # Return the generated image.
